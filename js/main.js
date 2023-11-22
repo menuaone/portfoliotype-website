ad714d06bc7566d54558e3cbc317380f455bd1ae
Menua Agamirov <menuaone@gmail.com>
2023-11-22 06:15:12 +0400

    almost finished js, 1 step left

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// появление всплювающего меню по нажатию на бургер
+
 const navBtn = document.querySelector(".nav-icon-btn");
 const navIcon = document.querySelector(".nav-icon");
 const nav = document.querySelector(".header__top-row");
@@ -9,3 +11,31 @@ navBtn.addEventListener('click', function() {
     // убрать скролл
     document.body.classList.toggle('no--scroll');
 })
+
+
+// отправка письма на почту по нажатию на кнопку формы
+function sendEmail() {
+
+    // обозначаем переменные, которые заходят через форму
+    let fname = document.getElementById("fname").value;
+    let mail = document.getElementById("email").value;
+    let message = document.getElementById("text").value;
+
+    let body = "Name: " + fname + "<br/> Email: " + mail + "<br/> Message: " + message;
+
+    // smtp API connection
+    Email.send({
+        SecureToken : "cb354adb-0384-4959-b7bb-23e4032cfc03",
+        To : 'mishutkaboss@gmail.com',
+        From : 'mishutkaboss@gmail.com',
+        Subject : "Message From Contact Form",
+        Body : body
+    }).then(
+    message => alert(message))
+
+};
+
+// по клику отправляем сообщение на почту
+const submitBtn = document.getElementById("btn");
+
+submitBtn.addEventListener('click', sendEmail)
